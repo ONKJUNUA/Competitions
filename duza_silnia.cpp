dcc040347d3ba7005b0c4ad9d41ef40506c0245e
ONKJUNUA <106984734+XiRO0O@users.noreply.github.com>
2023-02-05 18:53:31 +0100

    Create duza_silnia.cpp

---
+++ duza_silnia.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    switch(n){
+        case 1: cout << 1; break;
+        case 2: cout << 2; break;
+        case 3: cout << 6; break;
+        case 4: cout << 24; break;
+        case 5: cout << 120; break;
+        case 6: cout << 720; break;
+        case 7: cout << 5040; break;
+        case 8: cout << 40320; break;
+        case 9: cout << 362880; break;
+        case 10: cout << 3628800; break;
+        case 11: cout << 39916800; break;
+        case 12: cout << 479001600; break;
+        case 13: cout << 227020800; break;
+        case 14: cout << 178291200; break;
+        case 15: cout << 674368000; break;
+        case 16: cout << 789888000; break;
+        case 17: cout << 428096000; break;
+        case 18: cout << 705728000; break;
+        case 19: cout << 408832000; break;
+        case 20: cout << 176640000; break;
+        case 21: cout << 709440000; break;
+        case 22: cout << 607680000; break;
+        case 23: cout << 976640000; break;
+        case 24: cout << 439360000; break;
+        case 25: cout << 984000000; break;
+        case 26: cout << 584000000; break;
+        case 27: cout << 768000000; break;
+        case 28: cout << 504000000; break;
+        case 29: cout << 616000000; break;
+        case 30: cout << 480000000; break;
+        case 31: cout << 880000000; break;
+        case 32: cout << 160000000; break;
+        case 33: cout << 280000000; break;
+        case 34: cout << 520000000; break;
+        case 35: cout << 200000000; break;
+        case 36: cout << 200000000; break;
+        case 37: cout << 400000000; break;
+        case 38: cout << 200000000; break;
+        case 39: cout << 800000000; break;
+        default: cout << 0; break;
+    }
+    return 0;
+}
