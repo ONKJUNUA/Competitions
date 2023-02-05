b79dce21273b294a9be727e211feef319063ab07
ONKJUNUA <106984734+XiRO0O@users.noreply.github.com>
2023-02-05 18:53:12 +0100

    Create czy_sie_zatrzyma.cpp

---
+++ czy_sie_zatrzyma.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+    cin >> n;
+    while(n % 2 == 0) n = n / 2;
+    if(n <= 1) cout << "TAK";
+    else cout << "NIE";
+    return 0;
+}
