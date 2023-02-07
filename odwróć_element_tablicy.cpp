ae00baa79cca2d1e2b9e2c588e9271f8987435a9
ONKJUNUA <106984734+XiRO0O@users.noreply.github.com>
2023-02-07 23:34:29 +0100

    Create odwróć_element_tablicy.cpp

---
+++ odwróć_element_tablicy.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int tab[n];
+    for (int i=0;i<n;i++)
+        cin >> tab[i];
+    for (int i=n-1;i>=0;i--)
+        cout << tab[i] << " ";
+}
